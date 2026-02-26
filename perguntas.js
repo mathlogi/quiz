c13c4f7aa13266f29bfd2a2b789b3241221eacd0
mathlogi <mathlogi.explicacoes@gmail.com>
2026-02-26 12:09:57 +0000

    Create perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,21 @@
+const database = [
+    {
+        id: "mat_01",
+        title: "Matemática Interativa",
+        questions: [
+            { 
+                type: "multiple", 
+                q: "Qual a área de um triângulo com base 10 e altura 8?", 
+                img: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/Triangle.Area.svg/220px-Triangle.Area.svg.png", 
+                opts: ["80", "40", "20", "18"], 
+                ans: 1 
+            },
+            { 
+                type: "short", 
+                q: "Como se chama o polígono de 5 lados?", 
+                img: "", 
+                ans: "pentágono" 
+            }
+        ]
+    }
+];
