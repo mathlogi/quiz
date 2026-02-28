1a21633c787031b1a167f84d7c9da89cfdfda4a8
mathlogi <mathlogi.explicacoes@gmail.com>
2026-02-28 01:24:18 +0000

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -36,5 +36,101 @@ const database = [
                 ans: "pentágono" 
             }
         ]
-    }
+    },
+    {
+    "id": "mat9_01",
+    "title": "Monómios e Polinómios",
+    "questions": [
+        {
+            "type": "short",
+            "q": "Qual é o grau do monómio que representa a área de um quadrado de lado $x$?",
+            "img": "",
+            "ans": "2" 
+        },
+        {
+            "type": "short",
+            "q": "Indica o grau do polinómio: $-2x^2 + 3x^2 - 2x^2 + x + 1 - x - 3x^3$",
+            "img": "",
+            "ans": "3"
+        },
+        {
+            "type": "short",
+            "q": "Identifica o coeficiente do monómio $8x^2$:",
+            "img": "",
+            "ans": "8"
+        },
+        {
+            "type": "short",
+            "q": "Qual o grau do monómio $3x^2y^3$?",
+            "img": "",
+            "ans": "5"
+        },
+        {
+            "type": "short",
+            "q": "Escreve na forma reduzida o monómio: $2x \\cdot y^2 \\cdot \\frac{2x}{3}$",
+            "img": "",
+            "ans": "\\frac{4}{3}x^2y^2"
+        },
+        {
+            "type": "short",
+            "q": "Indica o grau do polinómio reduzido: $5x^3 + 3x^2 + 7y - 8x^2 + 12y - 5x^3$",
+            "img": "",
+            "ans": "2"
+        },
+        {
+            "type": "short",
+            "q": "Escreve na forma de polinómio reduzido: $3x(x - 1)$",
+            "img": "",
+            "ans": "3x^2-3x"
+        },
+        {
+            "type": "short",
+            "q": "Calcula o produto: $(x + 1)(2x + 3)$",
+            "img": "",
+            "ans": "2x^2+5x+3"
+        },
+        {
+            "type": "short",
+            "q": "Qual é a parte literal do monómio $-2ax^3y$?",
+            "img": "",
+            "ans": "x^3y"
+        },
+        {
+            "type": "short",
+            "q": "Simplifica a expressão: $2x + 3y - 3x + y$",
+            "img": "",
+            "ans": "-x+4y"
+        },
+        {
+            "type": "short",
+            "q": "Qual é o grau de um monómio constante não nulo (ex: $5$)?",
+            "img": "",
+            "ans": "0"
+        },
+        {
+            "type": "short",
+            "q": "Multiplica os monómios: $\\frac{1}{2}x \\cdot \\frac{2}{3}y^2 \\cdot 3x$",
+            "img": "",
+            "ans": "x^2y^2"
+        },
+        {
+            "type": "short",
+            "q": "Indica o coeficiente do monómio $\\frac{x}{2}$:",
+            "img": "",
+            "ans": "\\frac{1}{2}"
+        },
+        {
+            "type": "short",
+            "q": "Simplifica a expressão: $(2x - 3)(x - 1)$",
+            "img": " ",
+            "ans": "2x^2-5x+3"
+        },
+        {
+            "type": "short",
+            "q": "Escreve o polinómio reduzido equivalente a $(x-1)^2$:",
+            "img": "",
+            "ans": "x^2-2x+1"
+        }
+    ]
+}
 ];
