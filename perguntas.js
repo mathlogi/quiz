2de592a6dec11f70d8b04e9668fa7e13dca3ddc3
mathlogi <mathlogi.explicacoes@gmail.com>
2026-02-28 10:51:14 +0000

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -38,108 +38,284 @@ const database = [
         ]
     },
     {
-    "id": "mat9_mono_poli",
-    "title": "Monómios e Polinómios",
+    "id": "mat8_001",
+    "title": "Monômios e Polinómios ",
     "questions": [
         {
             "type": "multiple",
-            "q": "Qual é o grau do monómio que representa a área de um quadrado de lado $x$?",
-            "img": "",
-            "opts": ["$6$", "$5$", "$4$", "$2$"],
-            "ans": 3
-        },
-        {
-            "type": "multiple",
-            "q": "Qual é o grau do seguinte polinómio: $-2x^2 + 3x^2 - 2x^2 + x + 1 - x - 3x^3$?",
-            "img": "",
-            "opts": ["$0$", "$1$", "$2$", "$3$"],
-            "ans": 3
-        },
-        {
-            "type": "multiple",
-            "q": "Indica a simplificação da expressão: $-3x + 2y - 1 + x + \\frac{y}{2}$",
+            "q": "Qual é o grau do monómio que representa a área do quadrado da figura  $ABCD$  de lado  $x$ ?",
             "img": "",
             "opts": [
-                "$-2x + \\frac{5}{2}y - 1$",
-                "$-2x + \\frac{3}{2}y - 1$",
-                "$-4x + \\frac{5}{2}y - 1$",
+                "$6$",
+                "$5$",
+                "$4$",
+                "$2$"
+            ],
+            "ans": 3
+        },
+        {
+            "type": "multiple",
+            "q": "Qual é o grau do seguinte polinómio:  $-2x^2 + 3x^2 - 2x^2 + x + 1 - x - 3x^3$ ?",
+            "img": "",
+            "opts": [
+                "$0$",
+                "$1$",
+                "$2$",
+                "$3$"
+            ],
+            "ans": 3
+        },
+        {
+            "type": "multiple",
+            "q": "Das seguintes expressões, indica uma simplificação da expressão:  $-3x + 2y - 1 + x + \\\\frac{y}{2}$",
+            "img": "",
+            "opts": [
+                "$-2x + \\\\frac{5}{2}y - 1$",
+                "$-2x + \\\\frac{3}{2}y - 1$",
+                "$-4x + \\\\frac{5}{2}y - 1$",
                 "$-2x + 5y$"
             ],
             "ans": 0
         },
         {
             "type": "multiple",
-            "q": "Qual destas expressões NÃO é um monómio nem um polinómio?",
+            "q": "Qual das seguintes expressões não é um monómio nem um polinómio?",
             "img": "",
-            "opts": ["$xy \\cdot 3$", "$2x + \\frac{1}{2}$", "$3x + \\frac{1}{x}$", "$\\frac{1}{4}x$"],
+            "opts": [
+                "$xy \\\\cdot 3$",
+                "$2x + \\\\frac{1}{2}$",
+                "$3x + \\\\frac{1}{x}$",
+                "$\\\\frac{1}{4}x$"
+            ],
             "ans": 2
         },
         {
             "type": "multiple",
-            "q": "Indica o coeficiente do monómio $8x^2$:",
+            "q": "Qual é o polinómio na forma reduzida equivalente à expressão:  $x - \\\\frac{x-1}{3} + 2(x^2 + 1)$ ?",
             "img": "",
-            "opts": ["$x$", "$2$", "$8$", "$x^2$"],
+            "opts": [
+                "$2x^2 + \\\\frac{2}{3}x + \\\\frac{7}{3}$",
+                "$2x^2 + \\\\frac{2}{3}x + \\\\frac{5}{3}$",
+                "$2x^2 + 2x + 3$",
+                "$6x^2 + 2x + 7$"
+            ],
+            "ans": 0
+        },
+        {
+            "type": "multiple",
+            "q": "Qual das seguintes expressões representa o perímetro de um retângulo de lados  $x$  e  $y$ ?",
+            "img": "",
+            "opts": [
+                "$x + y$",
+                "$2x + 2y$",
+                "$xy$",
+                "$2x + y$"
+            ],
+            "ans": 1
+        },
+        {
+            "type": "multiple",
+            "q": "Qual é o valor numérico da expressão  $\\\\frac{1}{2}xy + 2$  para  $x = -1$  e  $y = \\\\frac{2}{3}$ ?",
+            "img": "",
+            "opts": [
+                "$-\\\\frac{5}{3}$",
+                "$\\\\frac{5}{3}$",
+                "$-\\\\frac{2}{3}$",
+                "$\\\\frac{2}{3}$"
+            ],
+            "ans": 1
+        },
+        {
+            "type": "multiple",
+            "q": "Qual das seguintes expressões é equivalente a  $(x - 2)(4 + 3x)$ ?",
+            "img": "",
+            "opts": [
+                "$3x^2 - (2x - 8)$",
+                "$-8x^2 - 6 + 11x^2 - 2 + 2x$",
+                "$2x - (-3x^2 + 8)$",
+                "$3x^2 - 2x - 8$"
+            ],
+            "ans": 3
+        },
+        {
+            "type": "multiple",
+            "q": "Identifica o coeficiente do monómio  $8x^2$ :",
+            "img": "",
+            "opts": [
+                "$x$",
+                "$2$",
+                "$8$",
+                "$x^2$"
+            ],
             "ans": 2
         },
         {
-            "type": "short",
-            "q": "Escreve na forma reduzida o monómio: $2x \\cdot y^2 \\cdot \\frac{2x}{3}$",
+            "type": "multiple",
+            "q": "Qual é o polinómio reduzido equivalente a  $(x - 1)^2$ ?",
             "img": "",
-            "ans": "\\frac{4}{3}x^2y^2"
+            "opts": [
+                "$x^2 - 1$",
+                "$x^2 - 2x + 1$",
+                "$x^2 + 1$",
+                "$x^2 + 2x + 1$"
+            ],
+            "ans": 1
+        },
+        {
+            "type": "multiple",
+            "q": "Dois monómios dizem-se simétricos se:",
+            "img": "",
+            "opts": [
+                "Tiverem a mesma parte literal",
+                "Tiverem coeficientes simétricos e a mesma parte literal",
+                "Tiverem o mesmo coeficiente",
+                "Tiverem graus diferentes"
+            ],
+            "ans": 1
+        },
+        {
+            "type": "multiple",
+            "q": "O monómio  $3xy$  e  $-\\\\frac{2}{3}xy$  são:",
+            "img": "",
+            "opts": [
+                "Simétricos",
+                "Semelhantes",
+                "Constantes",
+                "Nulos"
+            ],
+            "ans": 1
+        },
+        {
+            "type": "multiple",
+            "q": "Qual é o polinómio que representa a área de um retângulo de base  $(x+2)$  e altura  $(x+3)$ ?",
+            "img": "",
+            "opts": [
+                "$x^2 + 5x + 6$",
+                "$2x + 5$",
+                "$x^2 + 6$",
+                "$x^2 + 5$"
+            ],
+            "ans": 0
+        },
+        {
+            "type": "multiple",
+            "q": "Qual o grau do monómio  $3x^2y^3$ ?",
+            "img": "",
+            "opts": [
+                "$2$",
+                "$3$",
+                "$5$",
+                "$6$"
+            ],
+            "ans": 2
+        },
+        {
+            "type": "multiple",
+            "q": "Qual é a forma reduzida de  $(2 + 3x)(x - 1)$ ?",
+            "img": "",
+            "opts": [
+                "$3x^2 - x - 2$",
+                "$3x^2 + x - 2$",
+                "$3x^2 - 2$",
+                "$5x - 2$"
+            ],
+            "ans": 0
+        },
+        {
+            "type": "multiple",
+            "q": "Identifica a opção verdadeira sobre monómios semelhantes:",
+            "img": "",
+            "opts": [
+                "Têm o mesmo coeficiente",
+                "Têm a mesma parte literal",
+                "Têm o mesmo grau mas partes literais diferentes",
+                "São sempre iguais a zero"
+            ],
+            "ans": 1
         },
         {
             "type": "short",
-            "q": "Indica o grau do polinómio reduzido: $5x^3 + 3x^2 + 7y - 8x^2 + 12y - 5x^3$",
+            "q": "Escreve na forma reduzida o monómio:  $2x \\\\cdot y^2 \\\\cdot \\\\frac{2x}{3}$",
             "img": "",
-            "ans": "2"
+            "ans": "$\\\\frac{4}{3}x^2y^2$"
         },
         {
             "type": "short",
-            "q": "Escreve na forma de polinómio reduzido: $3x(x - 1)$",
+            "q": "Indica o grau do polinómio reduzido:  $5x^3 + 3x^2 + 7y - 8x^2 + 12y - 5x^3 + 0$",
             "img": "",
-            "ans": "3x^2-3x"
+            "ans": "$2$"
         },
         {
             "type": "short",
-            "q": "Calcula o produto: $(x + 1)(2x + 3)$",
+            "q": "Simplifica a expressão:  $2a + 3ab - a + \\\\frac{1}{2}a - \\\\frac{ab}{5}$",
             "img": "",
-            "ans": "2x^2+5x+3"
+            "ans": "$\\\\frac{3}{2}a + \\\\frac{14}{5}ab$"
         },
         {
             "type": "short",
-            "q": "Qual é a parte literal do monómio $-2ax^3y$?",
+            "q": "Escreve na forma de polinómio reduzido:  $3x(x - 1)$",
             "img": "",
-            "ans": "x^3y"
+            "ans": "$3x^2 - 3x$"
         },
         {
             "type": "short",
-            "q": "Simplifica: $2x + 3y - 3x + y$",
+            "q": "Calcula o produto:  $(x + 1)(2x + 3)$",
             "img": "",
-            "ans": "-x+4y"
+            "ans": "$2x^2 + 5x + 3$"
         },
         {
             "type": "short",
-            "q": "Escreve na forma reduzida: $(3x + 1)^2 - x^2$",
+            "q": "Reduz os termos semelhantes:  $\\\\frac{1}{2}x + \\\\frac{2}{3}y + \\\\frac{1}{2}x + \\\\frac{3}{10}y + \\\\frac{1}{2}$",
             "img": "",
-            "ans": "8x^2+6x+1"
+            "ans": "$x + \\\\frac{29}{30}y + \\\\frac{1}{2}$"
+        },
+        {
+            "type": "short",
+            "q": "Qual é la parte literal do monómio  $-2ax^3y$ ?",
+            "img": "",
+            "ans": "$x^3y$"
+        },
+        {
+            "type": "short",
+            "q": "Simplifica:  $2x + 3y - 3x + y$",
+            "img": "",
+            "ans": "$-x + 4y$"
+        },
+        {
+            "type": "short",
+            "q": "Escreve na forma reduzida:  $(3x + 1)^2 - x^2$",
+            "img": "",
+            "ans": "$8x^2 + 6x + 1$"
         },
         {
             "type": "short",
             "q": "Qual é o grau de um monómio constante não nulo?",
             "img": "",
-            "ans": "0"
+            "ans": "$0$"
         },
         {
             "type": "short",
-            "q": "Indica o coeficiente do monómio $\\frac{x}{2}$:",
+            "q": "Multiplica os monómios:  $\\\\frac{1}{2}x \\\\cdot \\\\frac{2}{3}y^2 \\\\cdot 3x$",
             "img": "",
-            "ans": "\\frac{1}{2}"
+            "ans": "$x^2y^2$"
         },
         {
             "type": "short",
-            "q": "Escreve na forma reduzida: $-2(x - \\frac{1}{2}) + 2y - (1 - y)$",
+            "q": "Indica o coeficiente do monómio  $\\\\frac{x}{2}$ :",
             "img": "",
-            "ans": "-2x+3y"
+            "ans": "$\\\\frac{1}{2}$"
+        },
+        {
+            "type": "short",
+            "q": "Escreve na forma reduzida:  $-2(x - \\\\frac{1}{2}) + 2y - (1 - y)$",
+            "img": "",
+            "ans": "$-2x + 3y$"
+        },
+        {
+            "type": "short",
+            "q": "Simplifica a expressão:  $(2x - 3)(x - 1)$",
+            "img": "",
+            "ans": "$2x^2 - 5x + 3$"
         }
     ]
 }
